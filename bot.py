d2d73c085af765bc466091326776c5fdcb850ca2
sluther <scott@focist.co>
2024-01-30 10:24:53 -0500

    move Undertaker

--- bot.py
+++ bot.py
@@ -1497,14 +1497,6 @@ class Storyteller(SeatingOrderModifier):
         return " - {}".format(self.role_name)
 
 
-class Undertaker(Townsfolk):
-    # The undertaker
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Undertaker"
-
-
 class Washerwoman(Townsfolk):
     # The washerwoman
 
