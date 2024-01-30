8f7ef3298958382c74be8f9004882e7e5b3af2eb
sluther <scott@focist.co>
2024-01-30 10:25:11 -0500

    move Washerwoman

--- bot.py
+++ bot.py
@@ -1497,14 +1497,6 @@ class Storyteller(SeatingOrderModifier):
         return " - {}".format(self.role_name)
 
 
-class Washerwoman(Townsfolk):
-    # The washerwoman
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Washerwoman"
-
-
 class Virgin(Townsfolk, NominationModifier):
     # The virgin
 
---
+++ characters/townsfolk/washerwoman.py
@@ -0,0 +1,9 @@
+from characters.types.townsfolk import Townsfolk
+
+
+class Washerwoman(Townsfolk):
+    # The washerwoman
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.role_name = "Washerwoman"
