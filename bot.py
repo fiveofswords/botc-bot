14cbbbc6d23b6f08acd3a1bf85ecc5c5bf56e1c8
sluther <scott@focist.co>
2024-01-30 10:26:35 -0500

    move Gambler

--- bot.py
+++ bot.py
@@ -1497,14 +1497,6 @@ class Storyteller(SeatingOrderModifier):
         return " - {}".format(self.role_name)
 
 
-class Gambler(Townsfolk):
-    # The gambler
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Gambler"
-
-
 class Gossip(Townsfolk):
     # The gossip
 
---
+++ characters/townsfolk/gambler.py
@@ -0,0 +1,9 @@
+from characters.types.townsfolk import Townsfolk
+
+
+class Gambler(Townsfolk):
+    # The gambler
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.role_name = "Gambler"
