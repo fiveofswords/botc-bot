79649f59d24cb9116beb9b2af151799b71af8a01
sluther <scott@focist.co>
2024-01-31 20:15:53 -0500

    move Baron

--- bot.py
+++ bot.py
@@ -1506,14 +1506,6 @@ class Storyteller(SeatingOrderModifier):
 # Outsiders
 
 
-class Baron(Minion):
-    # The baron
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Baron"
-
-
 class Assassin(Minion, DayStartModifier, DeathModifier):
     # The assassin
 
---
+++ characters/minion/baron.py
@@ -0,0 +1,9 @@
+from characters.types.minion import Minion
+
+
+class Baron(Minion):
+    # The baron
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.role_name = "Baron"
