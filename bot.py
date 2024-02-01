25418051e51fdca13e26d0152b8d158417eb8d2f
sluther <scott@focist.co>
2024-01-31 20:12:15 -0500

    move Mez

--- bot.py
+++ bot.py
@@ -1820,14 +1820,6 @@ class LilMonsta(Demon):
         self.role_name = "Lil' Monsta"
 
 
-class Mezepheles(Minion):
-    # The mezepheles
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Mezepheles"
-
-
 class Harpy(Minion):
     # The harpy
 
---
+++ characters/minion/mezepheles.py
@@ -0,0 +1,9 @@
+from characters.types.minion import Minion
+
+
+class Mezepheles(Minion):
+    # The mezepheles
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.role_name = "Mezepheles"
