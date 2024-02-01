305bec6edcd1d42ac718024e9134782122ec3367
sluther <scott@focist.co>
2024-01-31 20:18:31 -0500

    move Evil Twin

--- bot.py
+++ bot.py
@@ -1506,14 +1506,6 @@ class Storyteller(SeatingOrderModifier):
 # Outsiders
 
 
-class EvilTwin(Minion):
-    # The evil twin
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Evil Twin"
-
-
 class Cerenovus(Minion):
     # The cerenovus
 
