493da13532d8f5c79a2b07778b0e0df279365463
sluther <scott@focist.co>
2024-01-31 19:35:44 -0500

    move Philo

--- bot.py
+++ bot.py
@@ -1497,30 +1497,6 @@ class Storyteller(SeatingOrderModifier):
         return " - {}".format(self.role_name)
 
 
-class Philosopher(Townsfolk, AbilityModifier):
-    # The philosopher
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Philosopher"
-
-    def refresh(self):
-        super().refresh()
-        self.abilities = []
-
-    def add_ability(self, role):
-        is_set = False
-        for ability in self.abilities:
-            if isinstance(ability, AbilityModifier):
-                ability.add_ability(role)
-                is_set = True
-        if not is_set:
-            self.abilities = [role(self.parent)]
-
-    def extra_info(self):
-        return "\n".join([("Philosophering: {}\n{}".format(x.role_name, x.extra_info())) for x in self.abilities])
-
-
 class Sage(Townsfolk):
     # The sage
 
