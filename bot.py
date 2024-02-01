0de5e43dc9328d34dc27433b1062c8567d9bad0b
sluther <scott@focist.co>
2024-01-31 19:51:10 -0500

    move Amne

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ from config import *
 from dateutil.parser import parse
 
 from characters.basecharacter import BaseCharacter
+from characters.townsfolk.amnesiac import Amnesiac
 from characters.townsfolk.soldier import Soldier
 from characters.types.demon import Demon
 from characters.types.minion import Minion
@@ -2132,37 +2133,6 @@ class Leviathan(Demon):
     def __init__(self, parent):
         super().__init__(parent)
         self.role_name = "Leviathan"
-
-
-class Amnesiac(Townsfolk, VoteBeginningModifier, DayEndModifier):
-    # The amnesiac
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Amnesiac"
-        self.vote_mod = 1
-        self.player_with_votes = None
-
-    def extra_info(self):
-        if self.player_with_votes and self.vote_mod != 1:
-            return "{} votes times {}".format(self.player_with_votes.nick, self.vote_mod)
-        return super().extra_info()
-
-    def modify_vote_values(self, order, values, majority):
-        if self.player_with_votes and not self.isPoisoned and not self.parent.isGhost:
-            values[self.player_with_votes] = (values[self.player_with_votes][0], values[self.player_with_votes][1] * self.vote_mod)
-
-        return order, values, majority
-
-    def enhance_votes(self, player, multiplier):
-        self.player_with_votes = player
-        self.vote_mod = multiplier
-
-    def on_day_end(self):
-        self.vote_mod = 1
-        self.player_with_votes = None
-        super().on_day_end()
-
 
 
 class BountyHunter(Townsfolk):
---
+++ characters/townsfolk/amnesiac.py
@@ -0,0 +1,32 @@
+from bot import VoteBeginningModifier, DayEndModifier
+from characters.types.townsfolk import Townsfolk
+
+
+class Amnesiac(Townsfolk, VoteBeginningModifier, DayEndModifier):
+    # The amnesiac
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.role_name = "Amnesiac"
+        self.vote_mod = 1
+        self.player_with_votes = None
+
+    def extra_info(self):
+        if self.player_with_votes and self.vote_mod != 1:
+            return "{} votes times {}".format(self.player_with_votes.nick, self.vote_mod)
+        return super().extra_info()
+
+    def modify_vote_values(self, order, values, majority):
+        if self.player_with_votes and not self.isPoisoned and not self.parent.isGhost:
+            values[self.player_with_votes] = (values[self.player_with_votes][0], values[self.player_with_votes][1] * self.vote_mod)
+
+        return order, values, majority
+
+    def enhance_votes(self, player, multiplier):
+        self.player_with_votes = player
+        self.vote_mod = multiplier
+
+    def on_day_end(self):
+        self.vote_mod = 1
+        self.player_with_votes = None
+        super().on_day_end()
