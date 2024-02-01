6afb6a17c2ceb2cf51508169757c0db49e00cb58
sluther <scott@focist.co>
2024-01-31 19:58:18 -0500

    move Choirboy

--- bot.py
+++ bot.py
@@ -2159,14 +2159,6 @@ class Politician(Outsider):
         self.role_name = "Politician"
 
 
-class Choirboy(Townsfolk):
-    # The choirboy
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Choirboy"
-
-
 class Engineer(Townsfolk):
     # The engineer
 
