2ab37addb37cb9664fea819744ca0a6d895e4cda
sluther <scott@focist.co>
2024-01-31 19:50:24 -0500

    move Cannibal

--- bot.py
+++ bot.py
@@ -2110,26 +2110,6 @@ class Thief(Traveler, DayStartModifier, VoteBeginningModifier):
         return order, values, majority
 
 
-class Cannibal(Townsfolk, AbilityModifier):
-    # The cannibal
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Cannibal"
-
-    def add_ability(self, role):
-        is_set = False
-        for ability in self.abilities:
-            if isinstance(ability, AbilityModifier):
-                ability.add_ability(role)
-                is_set = True
-        if not is_set:
-            self.abilities = [role(self.parent)]
-
-    def extra_info(self):
-        return "\n".join([("Eaten: {}\n{}".format(x.role_name, x.extra_info())) for x in self.abilities])
-
-
 class Balloonist(Townsfolk):
     # The balloonist
 
