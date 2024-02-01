80e73dd79123ef9975896db5968854cda6623ce5
sluther <scott@focist.co>
2024-01-31 19:59:05 -0500

    move Magician

--- bot.py
+++ bot.py
@@ -2159,14 +2159,6 @@ class Politician(Outsider):
         self.role_name = "Politician"
 
 
-class Magician(Townsfolk):
-    # The magician
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Magician"
-
-
 class HighPriestess(Townsfolk):
     # The high priestess
 
