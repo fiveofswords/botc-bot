98b9c13466b317f3a7c230792e8858511c4875ac
sluther <scott@focist.co>
2024-01-31 19:37:40 -0500

    move Savant

--- bot.py
+++ bot.py
@@ -1497,14 +1497,6 @@ class Storyteller(SeatingOrderModifier):
         return " - {}".format(self.role_name)
 
 
-class Savant(Townsfolk):
-    # The savant
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Savant"
-
-
 class Seamstress(Townsfolk):
     # The seamstress
 
