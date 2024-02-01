63ddc96aeb4b862ea599c7eb394cc23faa187036
sluther <scott@focist.co>
2024-01-31 19:33:17 -0500

    move Artist

--- bot.py
+++ bot.py
@@ -1497,14 +1497,6 @@ class Storyteller(SeatingOrderModifier):
         return " - {}".format(self.role_name)
 
 
-class Artist(Townsfolk):
-    # The artist
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Artist"
-
-
 class Clockmaker(Townsfolk):
     # The clockmaker
 
