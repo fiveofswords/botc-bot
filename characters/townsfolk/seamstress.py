51d54f581bc7a1309478897a888f453879c481d3
sluther <scott@focist.co>
2024-01-31 19:37:58 -0500

    move Seamstress

---
+++ characters/townsfolk/seamstress.py
@@ -0,0 +1,9 @@
+from characters.types.townsfolk import Townsfolk
+
+
+class Seamstress(Townsfolk):
+    # The seamstress
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.role_name = "Seamstress"
