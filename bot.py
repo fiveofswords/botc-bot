b6a95b10bdad2da36c3ce0f39e9e718dc73ddd0f
sluther <scott@focist.co>
2024-01-31 20:08:33 -0500

    move Mutant

--- bot.py
+++ bot.py
@@ -1504,14 +1504,6 @@ class Storyteller(SeatingOrderModifier):
 # Outsiders
 
 
-class Mutant(Outsider):
-    # The mutant
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.role_name = "Mutant"
-
-
 class Sweetheart(Outsider):
     # The sweetheart
 
