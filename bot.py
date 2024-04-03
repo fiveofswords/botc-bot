6f15e16d720b692b7fe83065d2e29a02dc9e369d
Josh King <kingjoshdavid@gmail.com>
2024-04-03 11:45:24 -0400

    disable message tallies with capability to enable and disable them

--- bot.py
+++ bot.py
@@ -43,6 +43,7 @@ class Game:
             Player(Storyteller, STORYTELLER_ALIGNMENT, person, None)
             for person in gamemasterRole.members
         ] if not skip_storytellers else []
+        self.show_tally = False
 
     async def end(self, winner):
         # Ends the game
@@ -335,41 +336,42 @@ class Day:
                 # do not proceed with collecting user input for this vote
                 return
 
-        message_tally = {X: 0 for X in itertools.combinations(game.seatingOrder, 2)}
+        if(game.show_tally):
+            message_tally = {X: 0 for X in itertools.combinations(game.seatingOrder, 2)}
 
-        has_had_multiple_votes = len(self.votes) > 1
-        last_vote_message = None if not has_had_multiple_votes else await channel.fetch_message(self.votes[-2].announcements[0])
+            has_had_multiple_votes = len(self.votes) > 1
+            last_vote_message = None if not has_had_multiple_votes else await channel.fetch_message(self.votes[-2].announcements[0])
 
-        for person in game.seatingOrder:
-            for msg in person.messageHistory:
-                if msg["from"] == person:
-                    if has_had_multiple_votes:
-                        if msg["time"] >= last_vote_message.created_at:
-                            if (person, msg["to"]) in message_tally:
-                                message_tally[(person, msg["to"])] += 1
-                            elif (msg["to"], person) in message_tally:
-                                message_tally[(msg["to"], person)] += 1
-                            else:
-                                message_tally[(person, msg["to"])] = 1
-                    else:
-                        if msg["day"] == len(game.days):
-                            if (person, msg["to"]) in message_tally:
-                                message_tally[(person, msg["to"])] += 1
-                            elif (msg["to"], person) in message_tally:
-                                message_tally[(msg["to"], person)] += 1
-                            else:
-                                message_tally[(person, msg["to"])] = 1
-        sorted_tally = sorted(message_tally.items(), key=lambda x: -x[1])
-        messageText = "**Message Tally:**"
-        for pair in sorted_tally:
-            if pair[1] > 0:
-                messageText += "\n> {person1} - {person2}: {n}".format(
-                    person1=pair[0][0].nick, person2=pair[0][1].nick, n=pair[1]
-                )
-            else:
-                messageText += "\n> All other pairs: 0"
-                break
-        await safe_send(channel, messageText)
+            for person in game.seatingOrder:
+                for msg in person.messageHistory:
+                    if msg["from"] == person:
+                        if has_had_multiple_votes:
+                            if msg["time"] >= last_vote_message.created_at:
+                                if (person, msg["to"]) in message_tally:
+                                    message_tally[(person, msg["to"])] += 1
+                                elif (msg["to"], person) in message_tally:
+                                    message_tally[(msg["to"], person)] += 1
+                                else:
+                                    message_tally[(person, msg["to"])] = 1
+                        else:
+                            if msg["day"] == len(game.days):
+                                if (person, msg["to"]) in message_tally:
+                                    message_tally[(person, msg["to"])] += 1
+                                elif (msg["to"], person) in message_tally:
+                                    message_tally[(msg["to"], person)] += 1
+                                else:
+                                    message_tally[(person, msg["to"])] = 1
+            sorted_tally = sorted(message_tally.items(), key=lambda x: -x[1])
+            messageText = "**Message Tally:**"
+            for pair in sorted_tally:
+                if pair[1] > 0:
+                    messageText += "\n> {person1} - {person2}: {n}".format(
+                        person1=pair[0][0].nick, person2=pair[0][1].nick, n=pair[1]
+                    )
+                else:
+                    messageText += "\n> All other pairs: 0"
+                    break
+            await safe_send(channel, messageText)
 
         self.votes[-1].announcements.append(announcement.id)
         await self.votes[-1].call_next()
@@ -409,39 +411,40 @@ class Day:
         await safe_send(channel, "{}, go to sleep!".format(playerRole.mention))
 
         if not game.days[-1].riot_active:
-            message_tally = {X: 0 for X in itertools.combinations(game.seatingOrder, 2)}
-            has_had_multiple_votes = len(self.votes) > 0
-            last_vote_message = None if not has_had_multiple_votes else await channel.fetch_message(self.votes[-1].announcements[0])
-            for person in game.seatingOrder:
-                for msg in person.messageHistory:
-                    if msg["from"] == person:
-                        if has_had_multiple_votes:
-                            if msg["time"] >= last_vote_message.created_at:
-                                if (person, msg["to"]) in message_tally:
-                                    message_tally[(person, msg["to"])] += 1
-                                elif (msg["to"], person) in message_tally:
-                                    message_tally[(msg["to"], person)] += 1
-                                else:
-                                    message_tally[(person, msg["to"])] = 1
-                        else:
-                            if msg["day"] == len(game.days):
-                                if (person, msg["to"]) in message_tally:
-                                    message_tally[(person, msg["to"])] += 1
-                                elif (msg["to"], person) in message_tally:
-                                    message_tally[(msg["to"], person)] += 1
-                                else:
-                                    message_tally[(person, msg["to"])] = 1
-            sorted_tally = sorted(message_tally.items(), key=lambda x: -x[1])
-            messageText = "**Message Tally:**"
-            for pair in sorted_tally:
-                if pair[1] > 0:
-                    messageText += "\n> {person1} - {person2}: {n}".format(
-                        person1=pair[0][0].nick, person2=pair[0][1].nick, n=pair[1]
-                    )
-                else:
-                    messageText += "\n> All other pairs: 0"
-                    break
-            await safe_send(channel, messageText)
+            if (game.show_tally):
+                message_tally = {X: 0 for X in itertools.combinations(game.seatingOrder, 2)}
+                has_had_multiple_votes = len(self.votes) > 0
+                last_vote_message = None if not has_had_multiple_votes else await channel.fetch_message(self.votes[-1].announcements[0])
+                for person in game.seatingOrder:
+                    for msg in person.messageHistory:
+                        if msg["from"] == person:
+                            if has_had_multiple_votes:
+                                if msg["time"] >= last_vote_message.created_at:
+                                    if (person, msg["to"]) in message_tally:
+                                        message_tally[(person, msg["to"])] += 1
+                                    elif (msg["to"], person) in message_tally:
+                                        message_tally[(msg["to"], person)] += 1
+                                    else:
+                                        message_tally[(person, msg["to"])] = 1
+                            else:
+                                if msg["day"] == len(game.days):
+                                    if (person, msg["to"]) in message_tally:
+                                        message_tally[(person, msg["to"])] += 1
+                                    elif (msg["to"], person) in message_tally:
+                                        message_tally[(msg["to"], person)] += 1
+                                    else:
+                                        message_tally[(person, msg["to"])] = 1
+                sorted_tally = sorted(message_tally.items(), key=lambda x: -x[1])
+                messageText = "**Message Tally:**"
+                for pair in sorted_tally:
+                    if pair[1] > 0:
+                        messageText += "\n> {person1} - {person2}: {n}".format(
+                            person1=pair[0][0].nick, person2=pair[0][1].nick, n=pair[1]
+                        )
+                    else:
+                        messageText += "\n> All other pairs: 0"
+                        break
+                await safe_send(channel, messageText)
 
         await update_presence(client)
 
@@ -4934,7 +4937,14 @@ async def on_message(message):
                         messageText += "\n> All other pairs: 0"
                         break
                 await safe_send(channel, messageText)
-
+            elif command == "enabletally":
+                game.show_tally = True
+                for memb in game.storytellers:
+                    await safe_send(memb.user, "The message tally has been enabled by {}.".format(message.author.display_name))
+            elif command == "disabletally":
+                game.show_tally = False
+                for memb in game.storytellers:
+                    await safe_send(memb.user, "The message tally has been disabled by {}.".format(message.author.display_name))
             # Views relevant information about a player
             elif command == "info":
                 if game is NULL_GAME:
