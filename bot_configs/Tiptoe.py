e9d9b2e00d6086991bee59f14249f35129fc4cef
Josh King <kingjoshdavid@gmail.com>
2024-07-13 23:30:39 -0400

    add bot config files

---
+++ bot_configs/Tiptoe.py
@@ -0,0 +1,12 @@
+serverid = 421432788894482434
+channelid = 569953402159890462
+whisperchannelid = 1251899751356567593
+
+gamemasterName = 'Storyteller 2'
+playerName = 'Townsfolk 2 (Clocktower)'
+ghostName = 'Ghost 2 (Clocktower)'
+deadVoteName = 'Vote 2 (Clocktower)'
+travelerName = 'Traveller 2 (Clocktower)'
+observerName = 'Observer 2 (Clocktower)'
+inactiveName = 'Inactive'
+prefixes = ('@', ',')
