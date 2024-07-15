48e01e0628325dc12be09fd1101b338b6eed898f
Dae Lorant <DLorant@gmail.com>
2024-07-14 18:48:03 -0700

    Extracted out a base settings class to be shared logic for global and per-game settings.

--- model/settings/test_game_settings.py
+++ model/settings/test_game_settings.py
@@ -1,61 +1,48 @@
 import unittest
+
+from model.settings import game_settings
+from model.settings._base_settings import _BaseSettings
 from model.settings.game_settings import GameSettings
-import model.settings.game_settings
+
 import os
 
 
 class TestGameSettings(unittest.TestCase):
 
     def setUp(self):
-        self.original_filename = model.settings.game_settings._SETTINGS_FILENAME
-        model.settings.game_settings._SETTINGS_FILENAME = 'test_settings.json'
-        self.game_settings = GameSettings()
-        self.test_filename = model.settings.game_settings._SETTINGS_FILENAME
+        # Set up a GameSettings instance with some predefined settings
+        self.game_settings = GameSettings(
+            _BaseSettings(game_settings._SETTINGS_FILENAME, {1: {"st_channel": 12345}}))
 
     def tearDown(self):
-        if os.path.exists(self.test_filename):
-            os.remove(self.test_filename)
-        model.settings.game_settings._SETTINGS_FILENAME = self.original_filename
-
-    def test_init(self):
-        self.assertDictEqual(self.game_settings._settings, {})
-
-    def test_set_st_channel(self):
-        self.game_settings.set_st_channel(1, 12345)
-        self.assertEqual(self.game_settings._settings[1], {"st_channel": 12345})
+        # Delete the preferences.json file after each test
+        if os.path.exists(game_settings._SETTINGS_FILENAME):
+            os.remove(game_settings._SETTINGS_FILENAME)
 
     def test_get_st_channel(self):
-        self.game_settings._settings[1] = {"st_channel": 12345}
         self.assertEqual(self.game_settings.get_st_channel(1), 12345)
+        self.assertIsNone(self.game_settings.get_st_channel(2))
+
+    def test_set_st_channel_new_player(self):
+        self.game_settings.set_st_channel(2, 67890)
+        self.assertEqual(self.game_settings.get_st_channel(2), 67890)
+
+    def test_set_st_channel_existing_player(self):
+        self.game_settings.set_st_channel(1, 67890)
+        self.assertEqual(self.game_settings.get_st_channel(1), 67890)
 
     def test_get_unset_st_channel(self):
-        self.assertIsNone(self.game_settings.get_st_channel(1))
+        self.assertIsNone(self.game_settings.get_st_channel(2))
 
     def test_clear_st_channel(self):
-        self.game_settings._settings[1] = {"st_channel": 12345}
         self.game_settings.clear_st_channel(1)
-        self.assertNotIn("st_channel", self.game_settings._settings[1])
-
-    def test_update_settings_update_setting(self):
-        self.game_settings._update_settings(1, {"volume": 50})
-        self.game_settings._update_settings(1, {"volume": 100})
-        self.assertEqual(self.game_settings._settings[1], {"volume": 100})
-
-    def test_update_settings_additional_setting(self):
-        self.game_settings._update_settings(1, {"volume": 50})
-        self.game_settings._update_settings(1, {"surface_area": 25})
-        self.assertEqual(self.game_settings._settings[1], {"volume": 50, "surface_area": 25})
-
-    def test_load_no_file(self):
-        loaded_settings = GameSettings.load_from_file()
-        self.assertTrue(os.path.exists(self.test_filename))
-        self.assertDictEqual(loaded_settings._settings, {})
+        self.assertIsNone(self.game_settings.get_st_channel(1))
 
     def test_save_and_load(self):
-        self.game_settings.set_st_channel(1, 12345)
-        self.game_settings.save_to_file()
-        loaded_settings = GameSettings.load_from_file()
-        self.assertEqual(self.game_settings._settings, loaded_settings._settings)
+        # Test saving and loading settings
+        self.game_settings.save()
+        loaded_settings = GameSettings.load()
+        self.assertEqual(self.game_settings, loaded_settings)
 
 
 if __name__ == '__main__':
