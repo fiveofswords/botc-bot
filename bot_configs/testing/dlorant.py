ffcffa7af23d1995af7e612ee7cb000d63dff704
Dae Lorant <DLorant@gmail.com>
2024-07-15 12:04:34 -0700

    Added my personal configs to a testing directory under bot configs

---
+++ bot_configs/testing/dlorant.py
@@ -0,0 +1,22 @@
+# Guild ID for Game Server
+SERVER_ID = 261394705873502208  # Guild ID for Game Server
+
+# Channel IDs
+GAME_CATEGORY_ID = 1202456091057725550  # Category for in play player channels and game channels
+HANDS_CHANNEL_ID = 1260773800400912545  # Hands ST Channel
+OBSERVER_CHANNEL_ID = 1260773624101605466  # Observer Channel
+INFO_CHANNEL_ID = 1260775601413816320  # Info Channel
+WHISPER_CHANNEL_ID = 1256790559499092030  # Channel for player whispers
+TOWN_SQUARE_CHANNEL_ID = 1202456172989251584  # Town Square
+OUT_OF_PLAY_CATEGORY_ID = 1260776733674704997  # Category for out of play player channels
+
+# Role Names
+PLAYER_ROLE = "botc-player"
+TRAVELER_ROLE = "botc-traveler"
+GHOST_ROLE = "botc-ghost"
+DEAD_VOTE_ROLE = "botc-deadVote"
+STORYTELLER_ROLE = "botc-storyteller"
+INACTIVE_ROLE = "botc-inactive"
+OBSERVER_ROLE = "botc-observer"
+
+PREFIXES = (',', '@')
