4ebcc184b05af966e8ce8d493faff5848d2cd6ae
Dae Lorant <DLorant@gmail.com>
2025-02-24 23:50:34 -0800

    Updated tests to use pytest rather than unittest infrastructure.

--- model/settings/tests/test_global_settings.py
+++ model/settings/tests/test_global_settings.py
@@ -1,65 +1,61 @@
 import os
-import unittest
 
 from model.settings import GlobalSettings, global_settings
 from model.settings._base_settings import _BaseSettings
 
 
-class TestGlobalSettings(unittest.TestCase):
+class TestGlobalSettings:
 
-    def setUp(self):
+    def setup_method(self):
         # Set up a GameSettings instance with some predefined settings
         self.global_settings = GlobalSettings(
             _BaseSettings(global_settings._SETTINGS_FILENAME,
                           {1: {'aliases': {'TestAlias': 'AliasedCommand'}, 'defaultvote': [True, 30]}}))
 
-    def tearDown(self):
+    @staticmethod
+    def teardown_method():
         # Delete the preferences.json file after each test
         if os.path.exists(global_settings._SETTINGS_FILENAME):
             os.remove(global_settings._SETTINGS_FILENAME)
 
     def test_get_alias(self):
-        self.assertEqual(self.global_settings.get_alias(1, 'TestAlias'), 'AliasedCommand')
-        self.assertIsNone(self.global_settings.get_alias(1, 'NonExistentAlias'))
+        assert self.global_settings.get_alias(1, 'TestAlias') == 'AliasedCommand'
+        assert self.global_settings.get_alias(1, 'NonExistentAlias') is None
 
     def test_add_alias_new_player(self):
         self.global_settings.set_alias(2, 'NewAlias', "NewAliasedCommand")
-        self.assertEqual(self.global_settings.get_alias(2, 'NewAlias'), "NewAliasedCommand")
+        assert self.global_settings.get_alias(2, 'NewAlias') == "NewAliasedCommand"
 
     def test_add_alias_new_alias(self):
         self.global_settings.set_alias(1, 'NewAlias', "NewAliasedCommand")
-        self.assertEqual(self.global_settings.get_alias(1, 'NewAlias'), "NewAliasedCommand")
+        assert self.global_settings.get_alias(1, 'NewAlias') == "NewAliasedCommand"
 
     def test_add_alias_does_not_remove_old_alias(self):
         self.global_settings.set_alias(1, 'NewAlias', "NewAliasedCommand")
-        self.assertEqual(self.global_settings.get_alias(1, 'TestAlias'), "AliasedCommand")
+        assert self.global_settings.get_alias(1, 'TestAlias') == "AliasedCommand"
 
     def test_update_existing_alias(self):
         self.global_settings.set_alias(1, 'TestAlias', "NewAliasedCommand")
-        self.assertEqual(self.global_settings.get_alias(1, 'TestAlias'), "NewAliasedCommand")
+        assert self.global_settings.get_alias(1, 'TestAlias') == "NewAliasedCommand"
 
     def test_get_default_vote(self):
-        self.assertEqual(self.global_settings.get_default_vote(1), (True, 30))
-        self.assertIsNone(self.global_settings.get_default_vote(2))
+        assert self.global_settings.get_default_vote(1) == (True, 30)
+        assert self.global_settings.get_default_vote(2) is None
 
     def test_set_default_vote_existing_user(self):
         self.global_settings.set_default_vote(1, False, 15)
-        self.assertEqual(self.global_settings.get_default_vote(1), (False, 15))
+        assert self.global_settings.get_default_vote(1) == (False, 15)
 
     def test_set_default_vote_new_user(self):
         self.global_settings.set_default_vote(2, False, 15)
-        self.assertEqual(self.global_settings.get_default_vote(2), (False, 15))
+        assert self.global_settings.get_default_vote(2) == (False, 15)
 
     def test_clear_default_vote(self):
         self.global_settings.clear_default_vote(1)
-        self.assertIsNone(self.global_settings.get_default_vote(1))
+        assert self.global_settings.get_default_vote(1) is None
 
     def test_save_load(self):
         # Test saving and loading settings
         self.global_settings.save()
         loaded_settings = GlobalSettings.load()
-        self.assertEqual(self.global_settings, loaded_settings)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert self.global_settings == loaded_settings
--- time_utils/tests/test_time_utils.py
+++ time_utils/tests/test_time_utils.py
@@ -1,13 +1,17 @@
 from datetime import datetime, timezone, timedelta
-from unittest import TestCase, mock
+from unittest import mock
+
+import pytest
+
 from time_utils.time_utils import parse_deadline, _convert_to_timedelta, _round_datetime_to_nearest_half_hour
 
 
-class TestParseDeadline(TestCase):
+class TestParseDeadline:
 
-    def setUp(self):
+    @pytest.fixture(autouse=True)
+    def setup_method(self, request):
         patcher = mock.patch('time_utils.time_utils.datetime', autospec=True)
-        self.addCleanup(patcher.stop)
+        request.addfinalizer(patcher.stop)
         self.mock_datetime = patcher.start()
 
         # Ensure strptime and fromtimestamp methods work as expected
@@ -26,7 +30,7 @@ class TestParseDeadline(TestCase):
         expected = datetime(1988, 5, 20, 18, 0, tzinfo=timezone.utc)
         actual = parse_deadline(input_deadline)
 
-        self.assertEqual(expected, actual, "The parsed deadline does not match the expected value.")
+        assert expected == actual, "The parsed deadline does not match the expected value."
 
     def test_time_in_past_sets_next_day(self):
         # Set up a real datetime object for comparison
@@ -38,14 +42,14 @@ class TestParseDeadline(TestCase):
         expected = datetime(1988, 5, 21, 1, 0, tzinfo=timezone.utc)
         actual = parse_deadline(input_deadline)
 
-        self.assertEqual(expected, actual, "The parsed deadline does not match the expected value.")
+        assert expected == actual, "The parsed deadline does not match the expected value."
 
     def test_unix_timestamp(self):
         input_deadline = "580149960"
         expected = datetime(1988, 5, 20, 16, 46, tzinfo=timezone.utc)
         actual = parse_deadline(input_deadline)
 
-        self.assertEqual(expected, actual, "The parsed deadline does not match the expected value.")
+        assert expected == actual, "The parsed deadline does not match the expected value."
 
     def test_relative_time_rounds_up_to_nearest_half_hour(self):
         # Set up a real datetime object for comparison
@@ -57,7 +61,7 @@ class TestParseDeadline(TestCase):
         expected = datetime(1988, 5, 20, 19, 30, tzinfo=timezone.utc)
         actual = parse_deadline(input_deadline)
 
-        self.assertEqual(expected, actual, "The parsed deadline does not match the expected value.")
+        assert expected == actual, "The parsed deadline does not match the expected value."
 
     def test_relative_time_rounds_down_to_nearest_half_hour(self):
         # Set up a real datetime object for comparison
@@ -69,70 +73,70 @@ class TestParseDeadline(TestCase):
         expected = datetime(1988, 5, 20, 19, 30, tzinfo=timezone.utc)
         actual = parse_deadline(input_deadline)
 
-        self.assertEqual(expected, actual, "The parsed deadline does not match the expected value.")
+        assert expected == actual, "The parsed deadline does not match the expected value."
 
     def test_invalid_string_returns_none(self):
         input_deadline = "1223"
-        self.assertIsNone(parse_deadline(input_deadline))
+        assert parse_deadline(input_deadline) is None
 
 
-class TestConvertToTimeDelta(TestCase):
+class TestConvertToTimeDelta:
     def test_accepts_only_hours(self):
         input_deadline = "+2h"
-        self.assertEqual(_convert_to_timedelta(input_deadline), timedelta(hours=2, minutes=0))
+        assert _convert_to_timedelta(input_deadline) == timedelta(hours=2, minutes=0)
 
     def test_accepts_only_minute(self):
         input_deadline = "+30m"
-        self.assertEqual(_convert_to_timedelta(input_deadline), timedelta(hours=0, minutes=30))
+        assert _convert_to_timedelta(input_deadline) == timedelta(hours=0, minutes=30)
 
     def test_accepts_hours_and_minutes(self):
         input_deadline = "+3h30m"
-        self.assertEqual(_convert_to_timedelta(input_deadline), timedelta(hours=3, minutes=30))
+        assert _convert_to_timedelta(input_deadline) == timedelta(hours=3, minutes=30)
 
     def test_accepts_decimal(self):
         input_deadline = "+3.5h"
-        self.assertEqual(_convert_to_timedelta(input_deadline), timedelta(hours=3, minutes=30))
+        assert _convert_to_timedelta(input_deadline) == timedelta(hours=3, minutes=30)
 
     def test_missing_prefix_returns_none(self):
         input_deadline = "2h"
-        self.assertIsNone(_convert_to_timedelta(input_deadline))
+        assert _convert_to_timedelta(input_deadline) is None
 
     def test_just_plus_returns_none(self):
         input_deadline = "+"
-        self.assertIsNone(_convert_to_timedelta(input_deadline))
+        assert _convert_to_timedelta(input_deadline) is None
 
     def test_invalid_input_without_h_or_m_returns_none(self):
         input_deadline = "+dk3"
-        self.assertIsNone(_convert_to_timedelta(input_deadline))
+        assert _convert_to_timedelta(input_deadline) is None
 
     def test_invalid_input_with_h_or_m_returns_none(self):
         input_deadline = "+3kh"
-        self.assertIsNone(_convert_to_timedelta(input_deadline))
+        assert _convert_to_timedelta(input_deadline) is None
 
 
-class TestRoundDatetimeToHalfHour(TestCase):
+class TestRoundDatetimeToHalfHour:
     def test_rounds_up_to_hour(self):
         input_datetime = datetime(1988, 5, 20, 16, 46, tzinfo=timezone.utc)
         expected = datetime(1988, 5, 20, 17, 0, tzinfo=timezone.utc)
-        self.assertEqual(_round_datetime_to_nearest_half_hour(input_datetime), expected)
+        assert _round_datetime_to_nearest_half_hour(input_datetime) == expected
 
     def test_rounds_down_to_hour(self):
         input_datetime = datetime(1988, 5, 20, 16, 13, tzinfo=timezone.utc)
         expected = datetime(1988, 5, 20, 16, 0, tzinfo=timezone.utc)
-        self.assertEqual(_round_datetime_to_nearest_half_hour(input_datetime), expected)
+        assert _round_datetime_to_nearest_half_hour(input_datetime) == expected
 
     def test_rounds_up_to_half_hour(self):
         input_datetime = datetime(1988, 5, 20, 16, 25, tzinfo=timezone.utc)
         expected = datetime(1988, 5, 20, 16, 30, tzinfo=timezone.utc)
-        self.assertEqual(_round_datetime_to_nearest_half_hour(input_datetime), expected)
+        assert _round_datetime_to_nearest_half_hour(input_datetime) == expected
 
     def test_rounds_down_to_half_hour(self):
         input_datetime = datetime(1988, 5, 20, 16, 37, tzinfo=timezone.utc)
         expected = datetime(1988, 5, 20, 16, 30, tzinfo=timezone.utc)
-        self.assertEqual(_round_datetime_to_nearest_half_hour(input_datetime), expected)
+        assert _round_datetime_to_nearest_half_hour(input_datetime) == expected
 
 
 if __name__ == '__main__':
-    import unittest
+    import pytest
 
-    unittest.main()
+    pytest.main()
