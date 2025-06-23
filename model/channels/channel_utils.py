fb6cd9fc6c0abb7bd4ce3121338b7d0b15e84a5c
Josh King <kingjoshdavid@gmail.com>
2025-06-23 04:24:14 -0400

    Reduce noise: do not inform the Storytellers when channels are reordered

--- model/channels/channel_utils.py
+++ model/channels/channel_utils.py
@@ -9,12 +9,8 @@ from . import ChannelManager
 
 
 async def reorder_channels(st_channels: list[TextChannel]):
-    for st in global_vars.gamemaster_role.members:
-        await message_utils.safe_send(st, "Setting up channels for game...")
     result = await ChannelManager(client).setup_channels_in_order(st_channels)
     if not result:
         for st in global_vars.gamemaster_role.members:
             await message_utils.safe_send(st, "Failed to set up channels. Please review the channel order.")
         return
-    for st in global_vars.gamemaster_role.members:
-        await message_utils.safe_send(st, "Channels setup successfully!")
